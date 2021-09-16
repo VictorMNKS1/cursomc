75d5c8eb4670ad3ed4b2c872e9e6947a679c82f1
Victor Rafael <victorrafael.dev@gmail.com>
2021-09-16 14:46:20 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/victorrafael/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.victorrafael.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.victorrafael.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+}
