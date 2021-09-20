a57cc41f2cbdeac2c367c29ff57b9e0ed8cc080d
VictorMNKS1 <victorrafael.dev@gmail.com>
2021-09-20 17:09:52 -0300

    Pedido, EstadoPagamento, pagamento

---
+++ src/main/java/com/victorrafael/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.victorrafael.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.victorrafael.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+	
+}
